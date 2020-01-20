349bc4700811454a3d6ba6e6b5c3322a099e3658
Wes <wal2.designer@gmail.com>
2020-01-20 07:05:32 -0300

    Adjusted to move just one

--- adj_size_by_position.py
+++ adj_size_by_position.py
@@ -42,11 +42,11 @@ def resize_by(axis, act):
     thickness = bpy.context.object.dimensions[2]
     if(axis == "x"):
         if(act == 'shrink'):
-            bpy.context.object.dimensions[0]=(width-(thickness*2))
+            bpy.context.object.dimensions[0]=(width-(thickness))
             # bpy.context.object.location[0]=(locX+thickness)
             # bpy.context.object.location[1]=(locY+thickness)
         elif (act == 'expand'):
-            bpy.context.object.dimensions[0]=(width+(thickness*2))
+            bpy.context.object.dimensions[0]=(width+(thickness))
             # if(locX <= 0):
             #     bpy.context.object.location[0]=(0)
             # else:
@@ -58,11 +58,11 @@ def resize_by(axis, act):
             
     elif(axis == "y"):
         if(act == 'shrink'):
-            bpy.context.object.dimensions[1]=(height-(thickness*2))
+            bpy.context.object.dimensions[1]=(height-(thickness))
             # bpy.context.object.location[2]=(locZ+thickness)
 
         elif (act == 'expand'):
-            bpy.context.object.dimensions[1]=(height+(thickness*2))
+            bpy.context.object.dimensions[1]=(height+(thickness))
             # bpy.context.object.location[2]=(locZ-thickness)
             # if(locZ <= 0):
             #     bpy.context.object.location[2]=(0)
