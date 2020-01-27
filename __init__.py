c3eda7794d11ed0835b236574d5d64c479448196
Wes Alves <wal2.designer@gmail.com>
2020-01-27 13:37:55 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ bl_info = {
     "author" : "Wes",
     "description" : "",
     "blender" : (2, 80, 0),
-    "version" : (1, 0, 0),
+    "version" : (0, 1, 0),
     "location" : "",
     "warning" : "",
     "category" : "Generic"
