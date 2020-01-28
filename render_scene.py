e147dd06a0f9d6422c17d0935a76d29315adbfc9
Wes Alves <wal2.designer@gmail.com>
2020-01-28 12:26:17 -0300

    Wes/fix/import render scene (#29)
    * Update render_scene.py
    
    * Partially to test
    
    This has the functionality, but need to be tested
    
    * Adjs version to ver
    
    * adjs_working

--- render_scene.py
+++ render_scene.py
@@ -1,5 +1,6 @@
 import bpy
 import os
+import addon_utils
 
 class Render_studio_Operator(bpy.types.Operator):
     bl_idname = "object.render_studio"
@@ -20,13 +21,18 @@ class Attach_Operator(bpy.types.Operator):
 
 def r_studio():
     user = os.getlogin()
-    dir = "C:\\Users\\%s\\AppData\\Roaming\\Blender Foundation\\Blender\\2.81\\scripts\\addons\\takuara_addon" %(user)
-    myfile = "%s\\base_render.blend" %(dir)
-    files = []
-
-    with bpy.data.libraries.load(myfile) as (data_from, data_to):
-        data_to.scenes = ["render_scene"]
-        data_to.worlds = ['studio', 'artist_workshop', 'entrance_hall', 'varanda']
+    addon_utils.modules_refresh()
+    for mod in (addon_utils.addons_fake_modules):
+        location = addon_utils.addons_fake_modules[mod].bl_info['location']
+        if location == 'takuara_addon':
+            ver = "%s%s%s" %(addon_utils.addons_fake_modules[mod].bl_info['version'][0], addon_utils.addons_fake_modules[mod].bl_info['version'][1], addon_utils.addons_fake_modules[mod].bl_info['version'][2])
+            cVersion = ("%s-%s" %(location, ver))
+            dir = "C:\\Users\\%s\\AppData\\Roaming\\Blender Foundation\\Blender\\2.81\\scripts\\addons\\%s" %(user,cVersion)
+            myfile = "%s\\base_render.blend" %(dir)
+            files = []
+            with bpy.data.libraries.load(myfile) as (data_from, data_to):
+                data_to.scenes = ["render_scene"]
+                data_to.worlds = ['studio', 'artist_workshop', 'entrance_hall', 'varanda']
 
 
 def attach_scenes():
